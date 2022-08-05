dc6df8f8bd879e91eeb042e5c58fcdee1604f7d8
Sforza <katena.moskvina@bk.ru>
2022-08-05 15:44:39 +0600

    working with lists and arrays

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 import './styles/App.css';
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState("Текст в инпуте");
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JS', body: 'Description'},
+    {id: 2, title: 'JS 2', body: 'Description 2'},
+    {id: 3, title: 'JS 3', body: 'Description 3'},
+  ])
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'PT', body: 'Description'},
+    {id: 2, title: 'PT 2', body: 'Description 2'},
+    {id: 3, title: 'PT 3', body: 'Description 3'},
+  ])
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JS', body: 'Description'}}/>
+      <PostList posts={posts} title={'List posts JS'}/>
+      <PostList posts={posts2} title={'List posts PT'}/>
     </div>
   );
 }
