c74ff15e607bfeb44de0e8c3af8e4acbd65d92ff
Sforza <katena.moskvina@bk.ru>
2022-08-05 16:27:16 +0600

    fix UI library

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 import './styles/App.css';
 
 function App() {
@@ -10,16 +11,15 @@ function App() {
     {id: 2, title: 'JS 2', body: 'Description 2'},
     {id: 3, title: 'JS 3', body: 'Description 3'},
   ])
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'PT', body: 'Description'},
-    {id: 2, title: 'PT 2', body: 'Description 2'},
-    {id: 3, title: 'PT 3', body: 'Description 3'},
-  ])
 
   return (
     <div className="App">
+      <form>
+        <input type="text" placeholder="Title"/>
+        <input type="text" placeholder="Description"/>
+        <MyButton>Create post</MyButton>
+      </form>
       <PostList posts={posts} title={'List posts JS'}/>
-      <PostList posts={posts2} title={'List posts PT'}/>
     </div>
   );
 }
