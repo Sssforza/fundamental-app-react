6997a82427b33ff38fec65d477f82c1c733d63fb
Sforza <katena.moskvina@bk.ru>
2022-08-05 14:48:02 +0600

    styles and props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import Counter from './components/Counter';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
 
 function App() {
-  const [likes, setLikes] = useState(5)
-  const [value, setValue] = useState('Текст в инпуте')
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState("Текст в инпуте");
 
   return (
-    <div className='App'>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+    <div className="App">
+      <PostItem post={{id: 1, title: 'JS', body: 'Description'}}/>
     </div>
   );
 }
