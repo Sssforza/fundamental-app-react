ab86a361bbabe37a323f3f082d2bc1e0457c846d
Sforza <katena.moskvina@bk.ru>
2022-08-08 15:21:58 +0600

    communication between components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
 import Counter from "./components/Counter";
+import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import './styles/App.css';
-
+ 
 function App() {
   const [posts, setPosts] = useState([
     {id: 1, title: 'JS', body: 'Description'},
@@ -13,31 +14,13 @@ function App() {
     {id: 3, title: 'JS 3', body: 'Description 3'},
   ])
 
-  const [post, setPost] = useState ({title: '', body: ''});
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
   }
 
   return (
     <div className="App">
-      <form>
-        <MyInput
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})}
-          type="text" 
-          placeholder="Title"
-        />
-        <MyInput 
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})}
-          type="text" 
-          placeholder="Description"
-        />
-        <MyButton onClick={addNewPost} >Create post</MyButton>
-      </form>
+      <PostForm create={createPost}/>
       <PostList posts={posts} title={'List posts JS'}/>
     </div>
   );
