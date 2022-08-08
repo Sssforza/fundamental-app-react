924228a3c253b38f82668211b8cf707a20bf4b0e
Sforza <katena.moskvina@bk.ru>
2022-08-08 11:30:58 +0600

    form with object

--- src/App.js
+++ src/App.js
@@ -13,32 +13,26 @@ function App() {
     {id: 3, title: 'JS 3', body: 'Description 3'},
   ])
 
-  const [title, setTitle] = useState ('');
-  const [body, setBody] = useState ('');
+  const [post, setPost] = useState ({title: '', body: ''});
+
   const addNewPost = (e) => {
-    e.preventDefault();
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    setPosts([...posts, newPost]);
-    setTitle('');
-    setBody('');
+    e.preventDefault()
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
   }
 
   return (
     <div className="App">
       <form>
         <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value)}
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})}
           type="text" 
           placeholder="Title"
         />
         <MyInput 
-          value={body}
-          onChange={e => setBody(e.target.value)}
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})}
           type="text" 
           placeholder="Description"
         />
