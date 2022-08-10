7bb5088fe3a1d0c455c2051c0fcfaccb25090efb
Sforza <katena.moskvina@bk.ru>
2022-08-10 10:43:43 +0600

    modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostForm from "./components/PostForm";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/MyModal/MyModal";
 import PostFilter from "./components/UI/PostFilter";
 import './styles/App.css';
  
@@ -14,6 +15,7 @@ function App() {
     {id: 3, title: 'вв', body: 'аа'},
   ])
   const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
 
   const sortedPosts = useMemo(() => {
     console.log('work function getSortedPosts')
@@ -29,6 +31,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -37,7 +40,12 @@ function App() {
 
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create Post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}}></hr>
       <PostFilter 
         filter={filter}
